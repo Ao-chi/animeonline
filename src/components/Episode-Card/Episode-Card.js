988b6e38497e63e1e39488969cc8733dfb92a769
Ao-chi <106396818+Ao-chi@users.noreply.github.com>
2023-02-25 16:45:30 +0800

    added proxy on images

--- src/components/Episode-Card/Episode-Card.js
+++ src/components/Episode-Card/Episode-Card.js
@@ -2,12 +2,13 @@ import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const EpisodeCard = ({ data }) => {
+    const proxy = "https://cors.consumet.stream";
     return (
         <div className="episode-card">
             <div className="episode-card__image-wrapper">
                 <div className="episode-card__overlay"></div>
                 <FontAwesomeIcon className="episode-card__play" icon={faPlay}></FontAwesomeIcon>
-                <img src={data.image} alt={data.id} />
+                <img src={`${proxy}/${data.image}`} alt={data.id} />
             </div>
             <div className="episode-card__desc">
                 <p className="ep-title">{data.title}</p>
